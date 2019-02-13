43a2bf2fcd32ff1a1915dc49401a994f9810a252
Raydel Leyva Blanco <ray.blanco84@gmail.com>
2019-02-12 20:24:41 -0500

    Add SignalR
    Signed-off-by: Jorge Antonio Gómez Colombat <jorge.gomez@datys.cu>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component, HostListener } from '@angular/core';
-
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pos-front';
 
   /*@HostListener('document: keypress', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
     console.log('handleKeyboardEvent', event);
   }*/
+  private _hubConnection: HubConnection;
+
+  ngOnInit(): void {
+    this._hubConnection = new HubConnectionBuilder().withUrl('http://localhost:5000/events').build();
+    this._hubConnection
+      .start()
+      .then(() => console.log('Connection started!'))
+      .catch(err => console.log('Error while establishing connection :('));
+
+    this._hubConnection.on('paid-credit', (data) => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._hubConnection.stop();
+  }
 }
