5e842a996f69726a4b6e0f3b798525347b294f1f
tony <tonytunes2005@gmail.com>
2019-03-29 07:48:03 -0400

    add functionality of proxy configuration for change on hot url for api and events servers

--- src/app/utils/url.path.enum.ts
+++ src/app/utils/url.path.enum.ts
@@ -1,2 +1,2 @@
-export const baseURL = 'http://localhost:5000/api/1.0';
-export const WEBSOCKET = 'http://localhost:5000/events';
+export const baseURL = '/api/1.0';
+export const WEBSOCKET = '/events';
