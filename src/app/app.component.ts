fc580b89b37db63301c6bbdc8b6a257458e7df53
Raydel Leyva Blanco <ray.blanco84@gmail.com>
2019-10-27 12:46:18 -0400

    Comunicaion with websocker (SignalR) for scanner data
    Signed-off-by: tony <tonytunes2005@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
-import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
+import { WEBSOCKET } from 'src/app/utils/url.path.enum';
+import { ScannerData } from './models/scanner.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pos-front';
+  connection: HubConnection;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.connection = new HubConnectionBuilder()
+      .configureLogging(LogLevel.Information)
+      .withUrl('http://localhost:5000/events' /* WEBSOCKET */) // Para poder probar en ambiente de desarrollo, para producion poner la constante
+      .build();
+
+      this.connection.start().then(function () {
+      console.log('Connected!');
+    }).catch(function (err) {
+      return console.error(err.toString());
+    });
+
+    this.connection.on('scanner-event', (data: ScannerData) => {
+      // ScannerData tiene varios atributos, tienes que usar el upc
+      console.log(data.message);
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.connection.stop();
+
+  }
 }
---
+++ src/app/models/scanner.model.ts
@@ -0,0 +1,5 @@
+export interface ScannerData {
+  upc: string;
+  type?: string;
+  message: string;
+}
