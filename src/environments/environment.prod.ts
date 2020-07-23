453193d5ea55d231cf9a4f06e80d49266ea4f312
tony <tonytunes2005@gmail.com>
2020-07-23 11:51:29 -0500

    v2.44.8 bug fix show total on generic product modal for external payment card.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   WEBSOCKET: '/events',
   CLIENTVIEW: '/entity',
   serverURL: ENV_CONFIG.API_URL,
-  version: '2.44.7',
+  version: '2.44.8',
   screenH: 0
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   WEBSOCKET: '/events',
   CLIENTVIEW: '/entity',
   serverURL: 'http://localhost:5000',
-  version: '2.44.7',
+  version: '2.44.8',
   screenH: 0
 };
 
